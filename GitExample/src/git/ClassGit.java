851cc8b445d6c104d031dfd505a72ce053aacfe9
Jean-Benoit ROUSSAT <jean.benoit.roussat@gmail.com>
2019-11-29 11:35:38 +0100

    feature commit 2

--- GitExample/src/git/ClassGit.java
+++ GitExample/src/git/ClassGit.java
@@ -4,6 +4,11 @@ public class ClassGit {
 
 	public static void main(String[] args) {
 		System.out.println("classe main1");
+		System.out.println("classe main41");
+		System.out.println("classe main2");
+		System.out.println("classe main3");
+		System.out.println("classe main4");
+		System.out.println("classe main5");
 	}
 
 }
